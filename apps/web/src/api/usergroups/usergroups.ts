cb138d950fa817cad283249152b1b2ba83964767
hrustalq <n1k3f1t@gmail.com>
2024-09-21 23:43:07 +0800

    fix errors

--- apps/web/src/api/usergroups/usergroups.ts
+++
@@ -1,7 +0,0 @@
-import { apiClient } from '@/api';
-import { UserGroupPagedListDto } from './types';
-
-export const getUserGroups = async (branchId: string): Promise<UserGroupPagedListDto> => {
-  const response = await apiClient.get(`/usergroups/${branchId}`);
-  return response.data;
-};
